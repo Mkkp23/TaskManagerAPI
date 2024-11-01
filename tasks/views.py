0ac68c566047ce124de8cc738279b41b3e652ee2
Mkk23 <kazem.23.petrodi@gmail.com>
2024-11-01 10:15:14 +0330

    Add CRUD views for the Task model.

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+# local imports
+from tasks.models import Task
+from tasks.serializers import TaskSerializer
+
+
+# view for listing and creating tasks
+class TaskListCreateView(ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+# view for get, update and deleting tasks
+class TaskRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
