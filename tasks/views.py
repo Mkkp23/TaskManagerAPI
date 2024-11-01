2e53b5043b3e9dfbca93f9aebb69d2bff3b08b23
Mkk23 <kazem.23.petrodi@gmail.com>
2024-11-01 15:58:34 +0330

    Add pagination to list view

--- tasks/views.py
+++ tasks/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.pagination import PageNumberPagination
 
 # local imports
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
 
 
+# class to modify the pagination
+class TaskPagination(PageNumberPagination):
+    page_size = 5
+
+
 # view for listing and creating tasks
 class TaskListCreateView(ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    pagination_class = TaskPagination
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
