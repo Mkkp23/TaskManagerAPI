d8882b66c23df2a28d0324b0a59da3c2ef820354
Mkk23 <kazem.23.petrodi@gmail.com>
2024-11-01 10:38:58 +0330

    Add validation to TaskSerializer.

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,8 +1,18 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from tasks.models import Task
 
 
-class TaskSerializer(ModelSerializer):
+class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ["title", "description", "completed", "created_at"]
+
+    def validate_title(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("The title field cannot be empty.")
+        return value
+
+    def validate_description(self, value):
+        if value and len(value) < 10:
+            raise serializers.ValidationError("Description must be at least 10 chars.")
+        return value
